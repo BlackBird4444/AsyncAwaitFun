0dfdff6cc5536d0aaf757a2a83c5afd8332f91f8
caroline.gignac <caroline.gignac@tc.gc.ca>
2022-04-29 15:16:14 -0400

    new hero model

---
+++ Models/HeroModel.cs
@@ -0,0 +1,9 @@
+public class HeroModel{
+    public string Id {get; set;}
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public string Role {get; set;}
+    public string Secondary {get; set;}
+    public string Image {get; set;}  //API returns url to image
+    public string Difficulty {get; set;}
+}
