52995022135fa277fe3604a86b145a84862837e0
caroline.gignac <caroline.gignac@tc.gc.ca>
2022-06-07 06:43:36 -0400

    generics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,51 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using OverwatchAPI.Models;
+using System.Net;
 
 namespace OverwatchAPI.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    //TODO: user httpClient and make all actions async
+    //private readonly HttpClient _httpClient; 
+
+    private readonly HttpWebRequest _httpWebRequest;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-    }
+        //TODO: should be in program.cs and injected in controllers so they all use same instance
+        // service injection
+        //_httpClient = new HttpClient();  
+        
+          }
 
-    public IActionResult Index()
+    public IActionResult Index()  //TODO: make async later
     {
+        
+        try{
+            //HttpResponseMessage response = await _httpClient.GetAsync("https://overwatch-hero-api.herokuapp.com/api/v1/heroes");
+
+            HttpWebRequest _httpWebRequest = (HttpWebRequest)WebRequest.Create("https://overwatch-hero-api.herokuapp.com/api/v1/heroes"); //static method                                          
+            _httpWebRequest.Accept = "/";
+            //_httpWebRequest. AcceptEncoding
+            
+            WebResponse response = _httpWebRequest.GetResponse();
+            string text;
+            using (var sr = new StreamReader(response.GetResponseStream()))
+            {
+                text = sr.ReadToEnd();
+            }
+
+            _logger.LogInformation("Response = " + text);
+        }
+        catch(HttpRequestException ex)
+        {
+            _logger.LogError("Error Message: " + ex.Message);
+        }
+        
         return View();
     }
 
